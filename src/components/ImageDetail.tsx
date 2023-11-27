619d8733adcfcd911a34aa545cb0d2277c50a4cd
Denis Arlakov <darlakov@mail.ru>
2023-11-28 02:33:09 +0300

    fix

--- src/components/ImageDetail.tsx
+++ src/components/ImageDetail.tsx
@@ -1,6 +1,6 @@
-import { Photo } from '@/lib/types';
-import Image from 'next/image';
-import { FC } from 'react';
+import { Photo } from "@/lib/types";
+import Image from "next/image";
+import { FC } from "react";
 
 interface ImageDetailProps {
   photo: Photo;
@@ -9,11 +9,13 @@ const ImageDetail: FC<ImageDetailProps> = ({ photo }) => {
   return (
     <div>
       <Image
+        placeholder="blur"
+        blurDataURL={photo.urls.small}
         src={photo.urls.regular}
         width={1000}
         height={600}
         alt={photo.alt_description}
-        className='object-cover mx-auto'
+        className="object-cover mx-auto"
       />
     </div>
   );
